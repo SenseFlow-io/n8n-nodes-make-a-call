fbb7c6960a0e6c2d481aa24099f8e761459c3057
Filip Kubiš <fkubis@icloud.com>
2025-07-29 13:17:18 +0200

    fix paths, bump up version

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 export * from './dist/credentials/SenseFlowApi.credentials';
-export * from './nodes/GetCallStatus/SenseFlowGetCallStatus.node.ts';
-export * from './nodes/StartCall/SenseFlowStartCall.node.ts';
+export * from './nodes/GetCallStatus/SenseFlowGetCallStatus.node';
+export * from './nodes/StartCall/SenseFlowStartCall.node';
 
